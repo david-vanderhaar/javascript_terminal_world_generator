4246c1123f47cbb305fb0a657c693119897ae73a
David Vanderhaar <d.vanderhaarhunter@gmail.com>
2023-07-31 16:40:11 -0500

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,1 @@
-<script>
-  import { base } from "$app/paths";
-</script>
-
-<h1>sveltekit-gh-pages</h1>
-<p>Deployed to GitHub Pages</p>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
-<a href="{base}/about">About</a>
+<h1>Javascript World Generator</h1>
